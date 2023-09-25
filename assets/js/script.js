7b55871d664fb5a1491c1a46129f3f880f738966
Anthony Hrabowski <ajhrabowski@gmail.com>
2023-09-25 17:55:35 -0400

    Added API link,Event listener click funtion,Var for API Key, fetch api funtion, 5-day forcast text and flexbox container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var APIKey = "729fc5cf58d6f14307e8d05b5359b614";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+const API_KEY = "729fc5cf58d6f14307e8d05b5359b614";
 
-fetch(queryURL)
+//add click handler on search button and var to keep track of input box
+var APIURL = "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=729fc5cf58d6f14307e8d05b5359b614"
+var search = document.getElementById("searchbtn");
+
+
+
+
+search.addEventListener("click", function () {
+    //console.log("hi")
+    fetch(APIURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var weather = data.weather[0]
+        });
+
+})
+
