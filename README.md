112b34e44f543f8129957a4e2b2be74422d41dd1
Anthony Hrabowski <ajhrabowski@gmail.com>
2023-10-14 01:16:05 -0400

    Updated the README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # atmospheric-panel
+5-Day Weather Dashboard
+
+## Description
+
+- This project is from scratch project on a even-numbered week so we weren't given any starter code. Our goal was to create a Weather Dashboard using a 5-Day Weather Forcast API  to retrieve weather data for cities.
+
+## Installation
+N/A
+
+## Credits
+N/A
+
+## License
+MIT License
+
+Copyright (c) [2023] [Anthony Hrabowski]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ search.addEventListener("click", function () {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            getForcast(data.coord.lat,data.coord.lon)
+            getForcast(data.coord.lat, data.coord.lon)
             var weather = data.weather[0]
 
             // We capture the data from the API
@@ -30,7 +30,7 @@ search.addEventListener("click", function () {
             var temp = data.main.temp;
             var wind = data.wind.speed;
             var humid = data.main.humidity;
-            var todaysDate = (new Date(data.dt*1000)).toDateString()
+            var todaysDate = (new Date(data.dt * 1000)).toDateString()
             var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
             console.log("Data:", cityName, temp, humid)
             // UPDATE the HTML content on screen
@@ -39,17 +39,17 @@ search.addEventListener("click", function () {
             currentWind.textContent = wind;
             currentHumidity.textContent = humid;
             currentDate.textContent = todaysDate
-            
+
 
         });
 
- 
+
 })
 
 
-function getForcast(latitude,longitude){
-    
-    var APIURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&units=imperial&appid=729fc5cf58d6f14307e8d05b5359b614"
+function getForcast(latitude, longitude) {
+
+    var APIURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=729fc5cf58d6f14307e8d05b5359b614"
     fetch(APIURL)
         .then(response => response.json())
         .then(data => {
@@ -59,15 +59,15 @@ function getForcast(latitude,longitude){
 
             var forecastData = data.list;
             console.log("Data: ", forecastData);
-            for(let i = 0; i < forecastData.length; i++) {
-               // console.log(forecastData[i].dt_txt);  // --> "1923-10-01 12:00:00"
-               // console.log(forecastData[i].dt_txt.split());  // --> "cat" -> ['c', 'a', 't']
-               //                                                 INDEX           0         1
-               // console.log(forecastData[i].dt_txt.split(' '));  // --> ['1923-10-01', '12:00:00']
+            for (let i = 0; i < forecastData.length; i++) {
+                // console.log(forecastData[i].dt_txt);  // --> "1923-10-01 12:00:00"
+                // console.log(forecastData[i].dt_txt.split());  // --> "cat" -> ['c', 'a', 't']
+                //                                                 INDEX           0         1
+                // console.log(forecastData[i].dt_txt.split(' '));  // --> ['1923-10-01', '12:00:00']
                 let date = forecastData[i].dt_txt.split(' ')[0]
                 let current = forecastData[i].dt_txt.split(' ')[1]
 
-                if(current === '12:00:00') {
+                if (current === '12:00:00') {
                     filteredData.push(forecastData[i])
                 }
             }
@@ -75,60 +75,60 @@ function getForcast(latitude,longitude){
             console.log("Filtered data: ", filteredData)
             container.innerHTML = "";
             // Now that we have the ESSENTIAL DATA --> what do we do with it(?)
-            for(let i = 0; i < filteredData.length; i++) {
+            for (let i = 0; i < filteredData.length; i++) {
                 var newTemp = document.createElement("div")
-                newTemp.setAttribute("class","weather1")
+                newTemp.setAttribute("class", "weather1")
                 var cityEl = document.createElement("h2")
-                
+
                 newTemp.appendChild(cityEl)
                 var iconUrl = `https://openweathermap.org/img/w/${filteredData[i].weather[0].icon}.png`;
                 var weatherIcon = document.createElement('img')
-                weatherIcon.setAttribute('src',iconUrl)
+                weatherIcon.setAttribute('src', iconUrl)
 
-               
+
                 //var todaysDate = (dayjs(data.dt*1000)).format('M/D/YYYY');
                 var dateEl = document.createElement("h2")
-                dateEl.textContent = " " + (new Date(filteredData[i].dt*1000)).toDateString();
+                dateEl.textContent = " " + (new Date(filteredData[i].dt * 1000)).toDateString();
                 // var newDate = (new Date(data.dt*1000)).toDateString()
                 // dateEl.textContent = newDate
                 newTemp.appendChild(dateEl);
 
 
                 newTemp.appendChild(weatherIcon)
-                
+
                 var tempEl = document.createElement("h2")
                 tempEl.textContent = "Temp:" + filteredData[i].main.temp;  // --> DAta[INDEX_vALUE].FIELDnAME.nestedFieldName
                 newTemp.appendChild(tempEl);
 
-            
+
                 var windEl = document.createElement("h2")
-                windEl.textContent = "Wind; " +filteredData[i].wind.speed
+                windEl.textContent = "Wind; " + filteredData[i].wind.speed
                 newTemp.appendChild(windEl)
 
-                
+
                 var humidEl = document.createElement("h2")
-                humidEl.textContent = "Humidity: " + filteredData[i].main.humidity;  
+                humidEl.textContent = "Humidity: " + filteredData[i].main.humidity;
                 newTemp.appendChild(humidEl);
-                
-                
-            
+
+
+
                 //add dayjs dayjs(forecast.dt_txt).format('M/D/YYYY');
-            
-                
-                
-                
+
+
+
+
                 container.appendChild(newTemp)
-         
+
 
             }
             // UPDATE THE DOM with the new data we recieved from the API call 
 
 
             // IF we dynnmaicatlly Create new html element 
-                 /// we have to create the elemenet
-                 // we have to add attributes
-                 // we have to add data / textContent
-                 // we have to APPEND THE NEW ELEMENT TO AN EXISITING HTML ELEMENT (that we have reference to)
-        }); 
+            /// we have to create the elemenet
+            // we have to add attributes
+            // we have to add data / textContent
+            // we have to APPEND THE NEW ELEMENT TO AN EXISITING HTML ELEMENT (that we have reference to)
+        });
 }
 
